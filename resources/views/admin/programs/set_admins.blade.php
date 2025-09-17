8c6438cc6311acaac4d22f6a4c4e35b954e4c175
germanusk <germanuskeming@gmail.com>
2025-09-17 15:34:58 +0100

    ------

--- resources/views/admin/programs/set_admins.blade.php
+++ resources/views/admin/programs/set_admins.blade.php
@@ -22,16 +22,16 @@
                         <div class="col-sm-8 col-lg-9"><input type="text" class="form-control" name="dvc_academic" id="" value="{{ old('dvc_academic', $admins->dvc_academic??null) }}"></div>
                     </div>
                     <div class="row my-3">
-                        <div class="col-sm-4 col-lg-3 text-secondary text-capitalize">@lang('text.DVC_administration_and_finanace')</div>
-                        <div class="col-sm-8 col-lg-9"><input type="text" class="form-control" name="dvc_admin" id="" value="{{ old('dvc_admin', $admins->dvc_admin??null) }}"></div>
-                    </div>
-                    <div class="row my-3">
                         <div class="col-sm-4 col-lg-3 text-secondary text-capitalize">@lang('text.DVC_cooperation_and_innovation')</div>
                         <div class="col-sm-8 col-lg-9"><input type="text" class="form-control" name="dvc_coop" id="" value="{{ old('dvc_coop', $admins->dvc_coop??null) }}"></div>
                     </div>
                     <div class="row my-3">
                         <div class="col-sm-4 col-lg-3 text-secondary text-capitalize">@lang('text.word_registrar')</div>
                         <div class="col-sm-8 col-lg-9"><input type="text" class="form-control" name="registrar" required value="{{ old('registrar', $admins->registrar??null) }}"></div>
+                    </div>
+                    <div class="row my-3">
+                        <div class="col-sm-4 col-lg-3 text-secondary text-capitalize">@lang('text.DVC_administration_and_finanace')</div>
+                        <div class="col-sm-8 col-lg-9"><input type="text" class="form-control" name="dvc_admin" id="" value="{{ old('dvc_admin', $admins->dvc_admin??null) }}"></div>
                     </div>
                     <div class="d-flex justify-content-end py-2">
                         <button type="submit" class="btn btn-primary btn-xs rounded">@lang('text.word_update')</button>
